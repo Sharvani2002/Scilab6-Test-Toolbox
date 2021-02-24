1495c477df930107fda7a85294e1e39760674c02
Sharvani2002 <sharvanilaxmisomayaji@gmail.com>
2021-02-25 00:39:51 +0530

    Updated Part B

---
+++ macros/transpose.sci
@@ -0,0 +1,36 @@
+// Copyright (C) 2019 - IIT Bombay - FOSSEE
+//
+// This file must be used under the terms of the CeCILL.
+// This source file is licensed as described in the file COPYING, which
+// you should have received as part of this distribution.  The terms
+// are also available at
+// http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+// Author: Rupak Rokade
+// Organization: FOSSEE, IIT Bombay
+// Email: toolbox@scilab.in
+
+//Written by: Sharvani Laxmi Somayaji
+
+function [y] = transpose()
+    //   Calls a transpose function defined in C with given inputs and returns the expected result
+    //
+    //   Syntax
+    //   transpose_matrix = transpose(matrix, n, m)
+    //   
+    //   Parameters
+    //   transpose_matrix : ouput with m x n dimension
+    //   matrix : matrix to be transposed with n x m dimension
+    //   n : first dimension(number of rows of the matrix)
+    //   m : second dimension(number of columns of the matrix)
+    //   
+    //   Description
+    //   This function accepts accepts a matrix and finds its transpose
+    //
+    //
+    // Examples 
+    // // Transposing a matrix
+    // transpose_matrix = transpose([1,2,3;4,5,6], 2, 3)
+	//
+// Authors
+// Sharvani Laxmi Somayaji
+endfunction
---
+++ sci_gateway/cpp/sci_transpose.cpp
@@ -0,0 +1,59 @@
+// Copyright (C) 2019 - IIT Bombay - FOSSEE
+//
+// This file must be used under the terms of the CeCILL.
+// This source file is licensed as described in the file COPYING, which
+// you should have received as part of this distribution.  The terms
+// are also available at
+// http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+// Author: Rupak Rokade
+// Organization: FOSSEE, IIT Bombay
+// Email: toolbox@scilab.in
+
+//Written by:
+//Sharvani Laxmi Somayaji
+
+extern "C"
+{
+#include<Scierror.h>
+#include<api_scilab.h>
+#include <stdio.h>
+#include "localization.h"
+#include "trans.h"
+
+
+
+
+static const char fname[] = "transpose";
+int sci_transpose(scilabEnv env, int nin, scilabVar* in, int nopt, scilabOpt* opt, int nout, scilabVar* out)
+
+{
+	int n = 0;
+	int m = 0;
+    int size1 = 0;
+	double* transpose_matrix = NULL;
+	double* matrix = NULL;
+  
+if (nin < 3)
+    {
+        Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), fname, 3);
+        return 1;
+    }
+
+if (nout != 1)
+    {
+        Scierror(77, _("%s: Wrong number of output argument(s): %d expected.\n"), fname, 1);
+        return 1;
+    }
+    // scilab_getDoubleArray(env, in[1], &n);
+    // scilab_getDoubleArray(env, in[2], &m);
+    size1 = scilab_getDim2d(env, in[0], &n, &m);
+    scilab_getDoubleArray(env, in[0], &matrix);
+    out[0] = scilab_createDoubleMatrix2d(env, m, n,0);
+    scilab_getDoubleArray(env, out[0], &transpose_matrix);
+
+
+		transpose(matrix, transpose_matrix, n, m);
+
+    return 0;
+}
+}
